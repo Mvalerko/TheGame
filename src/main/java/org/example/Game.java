e085968f11b5ce6d94c33cb27cee957d3f58ee85
mvalerko <04445326@mail.ru>
2023-03-22 19:46:15 +0100

    Удалены неиспользуемые части кода, достигнуто полное покрытие тестами по бранчам с логикой

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -25,39 +25,15 @@ public class Game {
             this.optTwo = findByName(playerName2);
         }
 
-        if (findById(optOne.getId()) == null | findById(optTwo.getId()) == null) {
-            throw new NotRegisteredException("При попытке провести сражение возникла ошибка." +
-                    "Кто-то из игроков не зарегестрирован на турнире. Операция не выполнена.");
+        if (optOne.getStrength() < optTwo.getStrength()) {
+            return 2;
+        } else if (optOne.getStrength() > optTwo.getStrength()) {
+            return 1;
         } else {
-            if (optOne.getStrength() < optTwo.getStrength()) {
-                return 2;
-            } else if (optOne.getStrength() > optTwo.getStrength()) {
-                return 1;
-            } else {
-                return 0;
-            }
+            return 0;
         }
     }
 
-    public Player findById(int idOpt) {
-
-        for (Player product : playersList) {
-            if (matchesId(product, idOpt)) {
-                return product;
-            }
-        }
-        return null;
-    }
-
-    public boolean matchesId(Player product, int search) {
-        if (product.getId() == search) {
-            return true;
-        } else {
-            return false;
-        }
-        // или в одну строку:
-        // return product.getName().contains(search);
-    }
 
     public Player findByName(String nameOpt) {
 
@@ -70,7 +46,7 @@ public class Game {
     }
 
     public boolean matchesName(Player product, String search) {
-        if (product.getName().equals(search)){
+        if (product.getName().equals(search)) {
             return true;
         } else {
             return false;

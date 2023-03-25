ce92c06e5ee1342faa7dcb07f2e2ffc03a563e8f
mvalerko <04445326@mail.ru>
2023-03-25 15:55:34 +0100

    Исправляем последствия merge

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
-
     HashMap<String, Player> playersList = new HashMap<>();
 
     public void register(String name, Player item) {
@@ -12,30 +11,25 @@ public class Game {
         playersList.put(name, item);
     }
     public int round(String playerName1, String playerName2) {
-        Player optOne;
-        Player optTwo;
 
-        if (findByName(playerName1) == null) {
+         Player optOne;
+         Player optTwo;
+
+        if (!matchesName(playerName1)) {
             throw new NotRegisteredException("При попытке провести сражение возникла ошибка." +
                     "Кто-то из игроков не зарегестрирован на турнире. Операция не выполнена.");
         } else {
-
-            optOne = findByName(playerName1);
-
-        }
-        if (findByName(playerName2) == null) {
-            throw new NotRegisteredException("При попытке провести сражение возникла ошибка." +
-                    "Кто-то из игроков не зарегестрирован на турнире. Операция не выполнена.");
-        } else {
-
+            optOne = playersList.get(playerName1);
 
         }
+
         if (!matchesName(playerName2)) {
             throw new NotRegisteredException("При попытке провести сражение возникла ошибка." +
                     "Кто-то из игроков не зарегестрирован на турнире. Операция не выполнена.");
         } else {
 
             optTwo = playersList.get(playerName2);
+
         }
         if (optOne.getStrength() < optTwo.getStrength()) {
             return 2;
